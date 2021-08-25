7cd4bd723a550cd64e18e6beaef345e65fb98c1b
Leslie Owiti <leslieowiti@Leslies-Air.attlocal.net>
2021-08-25 19:49:13 -0400

    started working on splash page

---
+++ react-app/src/components/SplashPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const SplashPage = ()  => {
+    return (
+        <>
+            <h1>Welcome to baby Trainer!™</h1>
+            <div></div>
+        </>
+    )
+}
+
+export default SplashPage
