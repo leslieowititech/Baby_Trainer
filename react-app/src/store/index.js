5221e538268a4ab465985bc75ee33834a722d0cd
Leslie Owiti <leslieowiti@Leslies-Air.attlocal.net>
2021-08-27 12:41:21 -0400

    getting my babies

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
+import babyReducer from './baby';
 
 const rootReducer = combineReducers({
   session,
+  babies: babyReducer
 });
 
 
