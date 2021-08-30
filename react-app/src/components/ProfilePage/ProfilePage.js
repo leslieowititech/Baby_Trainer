664a785faa906cf05af9de4e057f32030725733f
Leslie Owiti <leslieowiti@Leslies-Air.attlocal.net>
2021-08-30 18:30:21 -0400

    Full CRUD for baby

--- react-app/src/components/ProfilePage/ProfilePage.js
+++ react-app/src/components/ProfilePage/ProfilePage.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
@@ -12,8 +12,24 @@ const ProfilePage = () => {
     const things = Object.values(useSelector(state => state.babies));
     const babies = things.filter(things => things?.user_id === user.id);
 
+    const [editmode, setEditMode] = useState(false);
+    const [name, setName] = useState('');
+    const [birthday, setBirthday] = useState(new Date());
+
+  
+
     const deleteSpecificBaby = (id) => {
         dispatch(babyActions.deleteABaby(id)) 
+    }
+
+    const payload = {
+        name,
+        birthday,
+        user_id: user.id
+    }
+    const editSpecificBaby = (e, id) => {
+        e.preventDefault()
+        dispatch(babyActions.editABaby(payload, id))
     }
 
     useEffect(() => {
@@ -38,12 +54,49 @@ const ProfilePage = () => {
                     {babies?.map(baby => (
                         <div key={baby.id} className='baby-item-details'>
                             <div className='baby-info'>
-                                <div className='info-item'>Baby name: {baby.name}</div>
-                                <div className='info-item'>Birthday: {baby.birthday}</div>
+                                {!editmode && 
+                                        <>
+                                            <div className='info-item'>Baby name: {baby.name}</div>
+                                            <div className='info-item'>Birthday: {baby.birthday}</div>
+                                        </>
+                                
+                                }
+                                {editmode && 
+                                
+                                    <form 
+                                            onSubmit={(e) => editSpecificBaby(e, baby.id)}
+                                            className='profile-page-form'
+                                            >
+                                        <input 
+                                                value={name}
+                                                onChange={(e) => setName(e.target.value)}
+                                                name='name'
+                                                className='profile-page-form-input'
+                                                placeholder={baby.name}
+
+                                                />
+                                        <input
+                                                value={birthday}
+                                                onChange={(e) => setBirthday(e.target.value)}
+                                                name='birthday'
+                                                type='date'
+                                                className='profile-page-form-input'
+
+                                        />
+                                        <button type='submit'>Save</button>
+                                    </form>
+                                }
+
+                               
                             </div>  
                             <div className='baby-buttons'>
-                                <button className='util-btn edit-btn'>&#9998;</button>
-                                <button onClick={() => deleteSpecificBaby(baby.id)} className='util-btn delete-btn'>&#128465;</button>
+                                <button 
+                                        className='util-btn edit-btn'
+                                        onClick={editmode ? () => setEditMode(false) : () => setEditMode(true)}
+                                        >&#9998;</button>
+                                <button 
+                                        onClick={() => deleteSpecificBaby(baby.id) } 
+                                        className='util-btn delete-btn'>&#128465;</button>
                             </div>                         
                         </div>
                     ))}
