7597ae6b91298ccb286b0ca0508f4c239a1eb7e3
Leslie Owiti <leslieowiti@Leslies-Air.attlocal.net>
2021-09-01 00:58:09 -0400

    last part of feed

--- react-app/src/components/LogModals/OptionModal/OptionModal.js
+++ react-app/src/components/LogModals/OptionModal/OptionModal.js
@@ -1,21 +1,42 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 
 import './OptionModal.css';
+import { Modal } from '../../../context/Modal';
+import TimerModal from '../TimerModal/TimerModal';
 
 const OptionModal = ({option1, option2, icon1, icon2, logType}) => {
+    const [showModal, setShowModal] = useState(false);
+    const myRef = useRef(null)
+
+    const handleClick = (e) => {
+        if (logType === 'Feed log'){
+            // console.log(option1)
+            // console.log(myRef.current)
+            // console.log(e.target.defaultValue)
+
+        }
+    }
+
     return (
         <div className='option-modal-container'>
-            <h2 className='option-modal-logType'>
+            <h2 className='option-modal-logType' >
                  {logType}
             </h2>
-            <div className='option-modal-item'>
-                <div className='option-modal-icon-container'>
-                    <img src={icon1} alt={icon1} className='option-modal-icon-container'/>
+            <div 
+                className='option-modal-item' 
+                onClick={handleClick}
+                defaultValue={option1}
+                >
+                <div className='option-modal-icon-container' >
+                    <img src={icon1} alt={icon1} className='option-modal-image' />
                 </div>
                 {option1}
             </div>
             <span>or</span>
-            <div className='option-modal-item'>
+            <div 
+                className='option-modal-item'
+                onClick={handleClick}
+                >
                 <div className='option-modal-icon-container'>
                     <img src={icon2} alt={icon2} className='option-modal-image'/>
                 </div>
