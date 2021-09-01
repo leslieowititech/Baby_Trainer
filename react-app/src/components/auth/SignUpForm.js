2a11ecc10a93d57a6a0f5efc7bcfe9389254b90b
Leslie Owiti <leslieowiti@Leslies-Air.attlocal.net>
2021-09-01 17:01:28 -0400

    Signup styled and modalized

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { Redirect } from 'react-router-dom';
+
 import { signUp } from '../../store/session';
+import './SignUpForm.css';
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
@@ -43,50 +45,51 @@ const SignUpForm = () => {
   }
 
   return (
-    <form onSubmit={onSignUp}>
+    <form onSubmit={onSignUp} className='sign-up-form'>
       <div>
         {errors.map((error, ind) => (
           <div key={ind}>{error}</div>
         ))}
-      </div>
-      <div>
-        <label>User Name</label>
+      </div>          
         <input
           type='text'
           name='username'
           onChange={updateUsername}
           value={username}
-        ></input>
-      </div>
-      <div>
-        <label>Email</label>
+          placeholder='Enter username'
+          className='login-form-input'
+        ></input>    
+    
         <input
           type='text'
           name='email'
           onChange={updateEmail}
           value={email}
+          placeholder='Enter your email'
+          className='login-form-input'
         ></input>
-      </div>
-      <div>
-        <label>Password</label>
+     
+      
         <input
           type='password'
           name='password'
           onChange={updatePassword}
           value={password}
+          placeholder='Enter password'
+          className='login-form-input'
         ></input>
-      </div>
-      <div>
-        <label>Repeat Password</label>
+  
         <input
           type='password'
           name='repeat_password'
           onChange={updateRepeatPassword}
           value={repeatPassword}
           required={true}
+          placeholder='Confirm Password'
+          className='login-form-input'
         ></input>
-      </div>
-      <button type='submit'>Sign Up</button>
+     
+      <button type='submit' className='form-login-btn'>Sign Up</button>
     </form>
   );
 };
