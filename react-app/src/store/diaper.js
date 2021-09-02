31fe4ef4910d9da622e1e7e42e5bfcefa6774563
Leslie Owiti <leslieowiti@Leslies-Air.attlocal.net>
2021-09-02 10:58:06 -0400

    add to heroku

---
+++ react-app/src/store/diaper.js
@@ -0,0 +1,64 @@
+const CREATE_DIAPER = 'diapers/CREATE_DIAPER';
+const GET_DIAPER = 'diapers/GET_DIAPER';
+const DELETE_DIAPER = 'diapers/DELETE_DIAPER';
+const EDIT_DIAPER = 'diapers/EDIT_DIAPER';
+
+const setDiaper = (diaper) => {
+    return {
+        type: CREATE_DIAPER,
+        payload: diaper
+    }
+}
+
+const getDiaper = (diaper) => {
+    return {
+        type: GET_DIAPER,
+        payload: diaper
+    }
+}
+
+const deleteDiaper = (id) => {
+    return {
+        type: DELETE_DIAPER,
+        payload: id
+    }
+}
+
+const editDiaper = (diaper) => {
+    return {
+        type: EDIT_DIAPER,
+        payload: diaper
+    }
+}
+
+const initialState = { diaper: null }
+
+const diaperReducer = (state = initialState, action) => {
+    let newState = {...state}
+
+    switch(action.type){
+        case GET_DIAPER:
+        case DELETE_DIAPER:
+        case CREATE_DIAPER:
+        case EDIT_DIAPER:
+        default:
+            return newState
+    }
+
+}
+
+export const getAllDiapers = () => async dispatch => {
+    const response = await fetch('/api/diapers/', {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    if(response.ok){
+        const data = await response.json();
+        dispatch(getDiaper(data));
+        return response
+    }
+}
+
+
+export default diaperReducer;
