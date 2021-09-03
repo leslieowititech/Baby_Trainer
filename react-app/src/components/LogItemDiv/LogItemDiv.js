be7073ebb97b243f9c401e95fef8c04f92c7d0a9
Leslie Owiti <leslieowiti@Leslies-Air.attlocal.net>
2021-09-02 22:26:04 -0400

    adding some messages to guide user

--- react-app/src/components/LogItemDiv/LogItemDiv.js
+++ react-app/src/components/LogItemDiv/LogItemDiv.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { useParams } from 'react-router';
 
 import './LogItemDiv.css';
 import { Modal } from '../../context/Modal';
@@ -6,11 +7,11 @@ import OptionModal from '../LogModals/OptionModal/OptionModal';
 
 const LogItemDiv = ({title, imgUrl, option1, option2, icon1, icon2, logType}) => {
     const [showModal, setShowModal] = useState(false);
-   
-
+   const { babyId } = useParams()
+ console.log(babyId, '_________logdivItem')
     return (
         <>
-        <div className='log-item' onClick={() => setShowModal(true)}>
+        <div className='log-item' onClick={babyId ? () => setShowModal(true) : () => alert('Please select a baby to start loggging')}>
             <div className='icon-container '>
                 <img src={imgUrl} alt={imgUrl} className='icon-image'/>
             </div>
